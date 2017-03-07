ff1958affcc2a869fe78916cba53a82e7207dc92
gianluca <gianluca@guinness>
2017-03-06 22:23:28 -0300

    update!

--- caravela-web/buildAndRunApp.sh
+++ caravela-web/buildAndRunApp.sh
@@ -1,8 +1,15 @@
-/opt/tomcat/bin/catalina.sh stop 
-rm -rf /home/gianluca/git/caravela/caravela-web/target/caravela*
-rm -rf /opt/tomcat/webapps/caravela*
+#!/bin/sh
+CARAVELA_HOME=/home/gianluca/Downloads/caravela
+CARAVELA_DEV=${PWD}
+
+echo $CARAVELA_HOME;
+echo $CARAVELA_DEV;
+ 
+$CARAVELA_HOME/tomcat/bin/shutdown.sh
+ 
+rm -rf $CARAVELA_DEV/target/caravela*
+rm -f $CARAVELA_HOME/lib/caravela.war
 mvn package 
-mv /home/gianluca/git/caravela/caravela-web/target/caravela.war /opt/tomcat/webapps/
-#/opt/tomcat/bin/catalina.sh jpda start
-/opt/tomcat/bin/catalina.sh run
+mv $CARAVELA_DEV/target/caravela.war $CARAVELA_HOME/lib/
+$CARAVELA_HOME/tomcat/bin/catalina.sh run
 
