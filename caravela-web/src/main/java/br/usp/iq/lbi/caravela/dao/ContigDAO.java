1b0c072f744d7532def612be0a6f773faeba89aa
gianlucamajor <gianlucamajor@gmail.com>
2017-12-26 01:26:38 -0200

    HOT-FIX: Removendo método não utilizado.

--- caravela-web/src/main/java/br/usp/iq/lbi/caravela/dao/ContigDAO.java
+++ caravela-web/src/main/java/br/usp/iq/lbi/caravela/dao/ContigDAO.java
@@ -12,7 +12,6 @@ public interface ContigDAO extends DAO<Contig> {
 	Long CountByContigBySampleAndTiiGreatherThan(Sample sample, Double tii);
 	List<Contig> FindByContigListTBRBySample(Sample sample, Double tii, Integer numberOfFeatures, TaxonomicRank taxonomicRank, Integer numberOfBoundaries, Double indexOfConsistencyTaxonomicByCountReads, Double indexOfVerticalConsistencyTaxonomic, Integer firstResult, Integer maxResult);
 	List<Contig> FindByContigBySample(Sample sample, Double tii, Integer numberOfFeatures, TaxonomicRank taxonomicRank, Integer numberOfBoundaries, Double indexOfConsistencyTaxonomicByCountReads, Double indexOfVerticalConsistencyTaxonomic);
-	List<Contig> FindByContigBySampleAndTiiGreatherThan(Sample sample, Double tii, Integer maxResult);
 	List<Contig> FindByContigBySampleAndTiiGreatherThan(Sample sample, Double tii, Integer startPosition, Integer maxResult);
 	Contig findContigBySampleAndContigReference(Sample sample, String contigReference);
 	List<Contig> FindContigBySampleOrderByContigSizeAndITGDesc(Sample sample);
--- caravela-web/src/main/java/br/usp/iq/lbi/caravela/dao/ContigDAOImpl.java
+++ caravela-web/src/main/java/br/usp/iq/lbi/caravela/dao/ContigDAOImpl.java
@@ -102,15 +102,7 @@ public class ContigDAOImpl extends DAOImpl<Contig> implements ContigDAO {
 		query.setParameter("contigReference", contigReference);
 		return (Contig) query.getSingleResult();
 	}
-	
-	
-	public List<Contig> FindByContigBySampleAndTiiGreatherThan(Sample sample, Double tii, Integer maxResult) {
-		TypedQuery<Contig> query = entityManager.createQuery("SELECT c FROM Contig c WHERE c.sample=:sample AND c.taxonomicIdentificationIndex >:tii ORDER by length(c.sequence) DESC,  c.taxonomicIdentificationIndex DESC", Contig.class);
-		List<Contig> contigList = query.setParameter("sample", sample)
-				.setParameter("tii", tii)
-				.setMaxResults(maxResult).getResultList();
-		return contigList;
-	}
+
 	
 	public List<Contig> FindContigBySampleOrderByContigSizeAndITGDesc(Sample sample) {
 		TypedQuery<Contig> query = entityManager.createQuery("SELECT c FROM Contig c WHERE c.sample=:sample ORDER by length(c.sequence) DESC,  c.taxonomicIdentificationIndex DESC", Contig.class);
