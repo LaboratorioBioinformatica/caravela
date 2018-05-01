62668cd43fdbf30bf0950bbad7e99ab97d928727
Gianluca Major <gianlucamajor@gmail.com>
2018-05-01 17:44:40 -0300

    creater zipper to decompress files .zip after upload. use zipper on upload

--- caravela-web/src/main/java/br/usp/iq/lbi/caravela/controller/UploadController.java
+++ caravela-web/src/main/java/br/usp/iq/lbi/caravela/controller/UploadController.java
@@ -1,33 +1,35 @@
 
 package br.usp.iq.lbi.caravela.controller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import javax.inject.Inject;
-
-import br.com.caelum.vraptor.Controller;
-import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
-import br.com.caelum.vraptor.Post;
-import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.*;
 import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.observer.upload.UploadSizeLimit;
 import br.com.caelum.vraptor.observer.upload.UploadedFile;
+import br.com.caelum.vraptor.validator.Validator;
 import br.com.caelum.vraptor.view.Results;
 import br.usp.iq.lbi.caravela.controller.auth.WebUser;
 import br.usp.iq.lbi.caravela.dao.SampleDAO;
 import br.usp.iq.lbi.caravela.dao.SampleFileDAO;
 import br.usp.iq.lbi.caravela.domain.SampleReporter;
-import br.usp.iq.lbi.caravela.model.FileStatus;
-import br.usp.iq.lbi.caravela.model.FileType;
-import br.usp.iq.lbi.caravela.model.Sample;
-import br.usp.iq.lbi.caravela.model.SampleFile;
+import br.usp.iq.lbi.caravela.infrastructure.Zipper;
+import br.usp.iq.lbi.caravela.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Controller
 public class UploadController {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
+	private static final String JSON = "application/json";
+	private static final String ZIP = "application/zip";
+	private static final String GZIP = "application/gzip";
+
 	private static final String DEFAULT_RANK_TO_REPORT = "genus";
 
 	private static final Double DEFAULT_TII_VALUE = new Double(0d);
@@ -39,19 +41,21 @@ public class UploadController {
 	private final SampleFileDAO sampleFileDAO;
 	private final SampleReporter sampleReporter;
 	private final Environment environment;
+	private final Validator validator;
 	
 	public UploadController() {
-		this(null, null, null, null, null, null);
+		this(null, null, null, null, null, null, null);
 	}
 	
 	@Inject
-	public UploadController(Result result, WebUser webUser, SampleDAO sampleDAO, SampleReporter sampleReporter, SampleFileDAO sampleFileDAO, Environment environment) {
+	public UploadController(Result result, WebUser webUser, SampleDAO sampleDAO, SampleReporter sampleReporter, SampleFileDAO sampleFileDAO, Environment environment, Validator validator) {
 		this.result = result;
 		this.webUser = webUser;
 		this.sampleDAO = sampleDAO;
 		this.sampleReporter = sampleReporter;
 		this.sampleFileDAO = sampleFileDAO;
 		this.environment = environment;
+		this.validator = validator;
 	}
 	
 	public void view(){
@@ -62,19 +66,67 @@ public class UploadController {
 	@Post
 	@UploadSizeLimit(sizeLimit=2024 * 1024 * 1024, fileSizeLimit=2024 * 1024 * 1024)
 	public void uploadSampleFile(UploadedFile file, Long sampleId) throws IOException{
-		
+
 		Sample sample = sampleDAO.load(sampleId);
-		
-		File fileToSaveUniqueFileName = File.createTempFile("sampleFile_", ".json", new File(getFullPathDirectoryUpload()));
+
+
+		file.getContentType();
+
+		final String fullPathDirectoryUpload = getFullPathDirectoryUpload();
+
+		final File fileTMP = new File(fullPathDirectoryUpload);
+
+		final File fileToSaveUniqueFileName = File.createTempFile("sampleTemporaryFile_", ".tmp", fileTMP);
 		file.writeTo(fileToSaveUniqueFileName);
-		
+		final String absolutePathFile = fileToSaveUniqueFileName.getAbsolutePath();
+
+		logger.info("Temporary file was created - " + fileToSaveUniqueFileName.getName());
+
+		final String contentType = file.getContentType();
+		logger.info("Application type: " + contentType);
+
+		if(JSON.equals(contentType)){
+			final File sampleFileName = File.createTempFile("sampleFile_", ".json", fileTMP);
+			fileToSaveUniqueFileName.renameTo(sampleFileName);
+			logger.info("File uploaded renamed to: " + sampleFileName);
+
+		}
+
+		if(ZIP.equals(contentType)){
+			final String sampleFileName = File.createTempFile("sampleFile", ".json", fileTMP).getName();
+			new Zipper().unZipIt(absolutePathFile, fullPathDirectoryUpload, sampleFileName);
+			logger.info("Unzip file: " + sampleFileName);
+
+			fileToSaveUniqueFileName.delete();
+			logger.info("Temporary file: " + absolutePathFile + "deleted.");
+
+		}
+
+//		else {
+//
+//			validator.addIf( ! JSON.equals(file.getContentType()),
+//					new SimpleMessage("application.type", "Invalid Application Type: " + file.getContentType(), Severity.WARN));
+//
+//			validator.onErrorForwardTo(this).view();
+//
+//		}
+
+
+		updateSampleToUploaded(sample, fileToSaveUniqueFileName);
+		result.use(Results.json()).from(sample).serialize();
+
+
+
+	}
+
+	private void updateSampleToUploaded(Sample sample, File fileToSaveUniqueFileName) {
 		SampleFile sampleFile = new SampleFile(sample, FileType.ALL_JSON, FileStatus.UPLOADED, "pier", fileToSaveUniqueFileName.getAbsolutePath());
 		sample.toUploaded();
 
+		logger.info("Sample " + sample.getName() + " change status to " + SampleStatus.UPLOADED);
+
 		sampleFileDAO.save(sampleFile);
 		sampleDAO.save(sample);
-		
-		result.use(Results.json()).from(sample).serialize();
 	}
 
 	private String getFullPathDirectoryUpload() {
