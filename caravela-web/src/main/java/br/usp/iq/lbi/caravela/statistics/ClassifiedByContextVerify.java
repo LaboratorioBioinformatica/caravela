8ca50308c51503b129bd05af30cca7d04ae1a1ec
Gianluca Major <gianlucamajor@gmail.com>
2018-10-11 08:46:19 -0300

    add feature to remove multi alignment e filter by ct, ctv and border.

--- caravela-web/src/main/java/br/usp/iq/lbi/caravela/statistics/ClassifiedByContextVerify.java
+++ caravela-web/src/main/java/br/usp/iq/lbi/caravela/statistics/ClassifiedByContextVerify.java
@@ -1,19 +1,56 @@
 package br.usp.iq.lbi.caravela.statistics;
 
+import java.io.File;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ClassifiedByContextVerify {
+
+
     public static void main(String[] args) {
 
-        Set<Result> classifiedByContext = ClassifiedByContextManagerFile
-                .getClassifiedByContextWithoutMultiAlignment("/home/gianluca/Downloads/report-14-129-ZOO-DIA78-CENTRIFUGE-genus.tsv");
 
-        System.out.println(classifiedByContext.size());
+        final Set<String> zc3bFiles = searchFiles("/home/gianluca/Downloads/report-caravela/zc3-b/", "");
 
+        zc3bFiles.forEach(file -> {
 
+            final Set<Result> classifiedByContext = ClassifiedByContextManagerFile
+                    .getClassifiedByContextWithoutMultiAlignment(file);
+            final Set<Result> softFilterResults = softFilter(classifiedByContext);
 
+            System.out.println(file + "\t" + softFilterResults.size());
 
+        });
 
     }
+
+    private static Set<Result> softFilter(Set<Result> classifiedByContext) {
+        Set<Result> classifiedByContextResult = new HashSet<>();
+
+        for (Result result: classifiedByContext) {
+            if(result != null && result.getCTGenus() >= 0.4F && result.getCTVGenus() >= 0.4F && result.getBorder() <= 1){
+                classifiedByContextResult.add(result);
+            }
+
+        }
+        return classifiedByContextResult;
+    }
+
+    private static Set<String> searchFiles(String path, String key) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        Set<String> foundFiles = new HashSet<>();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                if (listOfFiles[i].getName().contains(key)) {
+                    foundFiles.add(listOfFiles[i].getPath());
+                }
+            }
+        }
+
+        return foundFiles;
+    }
+
 }
 
